f2b2235dd80c4b5261d139b5b13af01cfe82266c
GodblessMe2 <oshindeinde@gmail.com>
2023-05-02 22:56:41 +0100

    Worked on the basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" 
+  Manage API authentication
+"""
+
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """ 
+      Create a class to manage the API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ 
+          Require authentication before proceeding
+        """
+        if not path or not excluded_paths:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        has_wildcard = any(x.endswith("*") for x in excluded_paths)
+        if not has_wildcard:
+            return path not in excluded_paths
+        for e in excluded_paths:
+            if e.endswith("*"):
+                if path.startswith(e[:-1]):
+                    return False
+            if path == e:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        if request:
+            return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ 
+           Change if user is current login
+        """
+        return None
