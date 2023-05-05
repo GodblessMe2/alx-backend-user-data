4738b8190323bb40e1cdb485dbf15a7327693153
GodblessMe2 <oshindeinde@gmail.com>
2023-05-05 13:11:17 +0100

    corrections

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Document import
 """
-from api.v1.views.users import *
-from api.v1.views.index import *
+from views.users import *
+from views.index import *
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
